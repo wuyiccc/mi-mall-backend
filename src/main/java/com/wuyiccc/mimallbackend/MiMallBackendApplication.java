de61f42c38310d47493155697f3e8c1f5f0d7ce1
wuyiccc <3406324191@qq.com>
2020-09-10 10:17:47 +0800

    mi mall 项目初始化

---
+++ src/main/java/com/wuyiccc/mimallbackend/MiMallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wuyiccc.mimallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiMallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wuyiccc/mimallbackend/MiMallBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wuyiccc.mimallbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class MiMallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
